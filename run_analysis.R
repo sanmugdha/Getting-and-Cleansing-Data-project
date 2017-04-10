158d9e622cfeba3a6ee1dac5c7c23edc7cf5587c
sanmugdha <sanmugdha@gmail.com>
2017-04-10 11:36:59 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,104 +1,50 @@
-path <- getwd()
-path
+##----Part 1:Merges the training and the test sets to create one data set-----
+## considering zip file is downloaded and saved under working directory
+unzip("getdata-projectfiles-UCI HAR Dataset.zip")
 
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-f <- "Dataset.zip"
-if (!file.exists(path)) {
-  dir.create(path)
-}
-download.file(url, file.path(path, f))
+## test data:
+XTest<- read.table("UCI HAR Dataset/test/X_test.txt")
+YTest<- read.table("UCI HAR Dataset/test/Y_test.txt")
+SubjectTest <-read.table("UCI HAR Dataset/test/subject_test.txt")
 
-pathfile<-file.path(path,f)
-unzip(pathfile)
+## train data:
+XTrain<- read.table("UCI HAR Dataset/train/X_train.txt")
+YTrain<- read.table("UCI HAR Dataset/train/Y_train.txt")
+SubjectTrain <-read.table("UCI HAR Dataset/train/subject_train.txt")
 
-pathIn <- file.path(path, "UCI HAR Dataset")
-list.files(pathIn, recursive = TRUE)
+## features and activity
+features<-read.table("UCI HAR Dataset/features.txt")
+activity<-read.table("UCI HAR Dataset/activity_labels.txt")
 
-dtSubjectTrain <- fread(file.path(pathIn, "train", "subject_train.txt"))
-dtSubjectTest <- fread(file.path(pathIn, "test", "subject_test.txt"))
+##Part1 - merges train and test data in one dataset (full dataset at the end)
+X<-rbind(XTest, XTrain)
+Y<-rbind(YTest, YTrain)
+Subject<-rbind(SubjectTest, SubjectTrain)
 
-dtActivityTrain <- fread(file.path(pathIn, "train", "Y_train.txt"))
-dtActivityTest <- fread(file.path(pathIn, "test", "Y_test.txt"))
+##----Part 2:Extracts only the measurements on the mean and standard deviation for each measurement-----
+index<-grep("mean\\(\\)|std\\(\\)", features[,2]) ##getting features indeces which contain mean() and std() in their name
+length(index) ## count of features
+X<-X[,index] ## getting only variables with mean/stdev
+dim(X) ## checking dim of subset 
 
-fileToDataTable <- function(f) {
-  df <- read.table(f)
-  dt <- data.table(df)
-}
-dtTrain <- fileToDataTable(file.path(pathIn, "train", "X_train.txt"))
-dtTest <- fileToDataTable(file.path(pathIn, "test", "X_test.txt"))
+##----Part 3:Uses descriptive activity names to name the activities in the data set-----
+Y[,1]<-activity[Y[,1],2] ## replacing numeric values with lookup value from activity.txt; won't reorder Y set
+head(Y) 
 
-##Merges the training and the test sets to create one data set
+##----Part 4:Appropriately labels the data set with descriptive variable names-----
+names<-features[index,2] ## getting names for variables
 
-dtSubject <- rbind(dtSubjectTrain, dtSubjectTest)
-setnames(dtSubject, "V1", "subject")
-dtActivity <- rbind(dtActivityTrain, dtActivityTest)
-setnames(dtActivity, "V1", "activityNum")
-dt <- rbind(dtTrain, dtTest)
+names(X)<-names ## updating colNames for new dataset
+names(Subject)<-"SubjectID"
+names(Y)<-"Activity"
 
-dtSubject <- cbind(dtSubject, dtActivity)
-dt <- cbind(dtSubject, dt)
+CleanedData<-cbind(Subject, Y, X)
+head(CleanedData[,c(1:4)]) ## first 5 columns
 
-setkey(dt, subject, activityNum)
+##----Part 5:From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+CleanedData<-data.table(CleanedData)
+TidyData <- CleanedData[, lapply(.SD, mean), by = 'SubjectID,Activity'] ## features average by Subject and by activity
+dim(TidyData)
 
-##Extracts only the measurements on the mean and standard deviation for each measurement
-
-dtFeatures <- fread(file.path(pathIn, "features.txt"))
-setnames(dtFeatures, names(dtFeatures), c("featureNum", "featureName"))
-
-dtFeatures <- dtFeatures[grepl("mean\\(\\)|std\\(\\)", featureName)]
-
-dtFeatures$featureCode <- dtFeatures[, paste0("V", featureNum)]
-head(dtFeatures)
-
-select <- c(key(dt), dtFeatures$featureCode)
-dt <- dt[, select, with = FALSE]
-
-##Use descriptive activity names
-
-dtActivityNames <- fread(file.path(pathIn, "activity_labels.txt"))
-setnames(dtActivityNames, names(dtActivityNames), c("activityNum", "activityName"))
-
-##Appropriately labels the data set with descriptive variable names.
-
-dt <- merge(dt, dtActivityNames, by = "activityNum", all.x = TRUE)
-
-setkey(dt, subject, activityNum, activityName)
-
-dt <- data.table(melt(dt, key(dt), variable.name = "featureCode"))
-
-dt <- merge(dt, dtFeatures[, list(featureNum, featureCode, featureName)], by = "featureCode", all.x = TRUE)
-
-dt$activity <- factor(dt$activityName)
-dt$feature <- factor(dt$featureName)
-
-grepthis <- function(regex) {
-  grepl(regex, dt$feature)
-}
-## Features with 2 categories
-n <- 2
-y <- matrix(seq(1, n), nrow = n)
-x <- matrix(c(grepthis("^t"), grepthis("^f")), ncol = nrow(y))
-dt$featDomain <- factor(x %*% y, labels = c("Time", "Freq"))
-x <- matrix(c(grepthis("Acc"), grepthis("Gyro")), ncol = nrow(y))
-dt$featInstrument <- factor(x %*% y, labels = c("Accelerometer", "Gyroscope"))
-x <- matrix(c(grepthis("BodyAcc"), grepthis("GravityAcc")), ncol = nrow(y))
-dt$featAcceleration <- factor(x %*% y, labels = c(NA, "Body", "Gravity"))
-x <- matrix(c(grepthis("mean()"), grepthis("std()")), ncol = nrow(y))
-dt$featVariable <- factor(x %*% y, labels = c("Mean", "SD"))
-## Features with 1 category
-dt$featJerk <- factor(grepthis("Jerk"), labels = c(NA, "Jerk"))
-dt$featMagnitude <- factor(grepthis("Mag"), labels = c(NA, "Magnitude"))
-## Features with 3 categories
-n <- 3
-y <- matrix(seq(1, n), nrow = n)
-x <- matrix(c(grepthis("-X"), grepthis("-Y"), grepthis("-Z")), ncol = nrow(y))
-dt$featAxis <- factor(x %*% y, labels = c(NA, "X", "Y", "Z"))
-
-setkey(dt, subject, activity, featDomain, featAcceleration, featInstrument, 
-       featJerk, featMagnitude, featVariable, featAxis)
-dtTidy <- dt[, list(count = .N, average = mean(value)), by = key(dt)]
-
-##Independent tidy data set with the average of each variable for each activity and each subject
-
-f <- file.path(path, "DatasetHumanActivityRecognitionUsingSmartphones.txt")
-write.table(dtTidy, f, quote = FALSE, sep = "\t", row.names = FALSE)
+head(TidyData[order(SubjectID)][,c(1:4), with = FALSE],12) 
+write.table(TidyData, file = "Tidy.txt", row.names = FALSE)
